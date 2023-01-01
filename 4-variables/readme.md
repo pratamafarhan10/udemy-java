b8758e382e868761b498b575442c5ed7f3acb073
pratamafarhan10 <pratamafarhan10@gmail.com>
2023-01-01 22:40:27 +0700

    section 4 done

--- 4-variables/readme.md
+++ 4-variables/readme.md
@@ -10,3 +10,4 @@ There are a lot of variable types in java
 ## Notes
 - Char always use 2 bytes of memory. For example if we store 'F' in a char it's 2 bytes, if we store in a string "F" it's 48 bytes.
 - Never use in for any math calculation
+- Java works from left to right, so if we have "numbers " + 20 + 21 it will print out "numbers 2021"
--- workbook/Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStore.java
+++ workbook/Module 1 - Introduction to Java/2. Variables/Workbook 2.3/AppleStore.java
@@ -7,19 +7,30 @@ public class AppleStore {
         // Instructions for this workbook are on Learn the Part (see the Udemy Video: Workbook 2.3 to access the link).
 
         System.out.println("You picked 500 apples from an apple orchard");
+        numOfApples += 500;
         
         System.out.println("Time for business! You're selling each apple for 40 cents");
+        int price = 40;
 
         System.out.println("One customer walked in. He bought 4 apples!");
+        numOfCustomers++;
+        profit += 4 * price;
+        numOfApples -= 4;
 
         System.out.println("Another customer walked in. He bought 20 apples!");
+        numOfCustomers++;
+        profit += 20 * price;
+        numOfApples -= 20;
 
         System.out.println("Another customer walked in. She bought 200 apples!");
+        numOfCustomers++;
+        profit += 200 * price;
+        numOfApples -= 200;
         
 
-        System.out.println("Wow! So far, you made: $<profit>");
-        System.out.println("<number of customers> + customers love your apples.");
-        System.out.println("You have <number of apples> apples left.");
+        System.out.println("Wow! So far, you made: $" + (profit/100));
+        System.out.println(numOfCustomers + " customers love your apples.");
+        System.out.println("You have "+ numOfApples + " apples left.");
 
         // Compare your result to what's on Learn the Part.  
         
